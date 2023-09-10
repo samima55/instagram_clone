64f706e8b53e8e4eba6fbc5bfe61ea7cc3525e8d
Samima Hassan <samimahassan55@gmail.com>
2023-09-10 20:19:46 +0200

    header ADDED

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App_header'>
+        <img className='App_headerImage'
+         src='https://1000logos.net/wp-content/uploads/2017/02/Logo-Instagram.png'
+          alt='logo'
+          />
+         </div>
+      <h1>hello instagram clone</h1>
+      {/*header */}
+       {/* post*/}
+        {/*post */}
     </div>
   );
 }
