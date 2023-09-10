2cd222091dba543cf5ca8013a0f2518f4aa1e522
Samima Hassan <samimahassan55@gmail.com>
2023-09-10 21:13:14 +0200

    header component added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from './Components/HeaderComponent/Header';
 function App() {
   return (
-    <div className="App">
-      <div className='App_header'>
-        <img className='App_headerImage'
-         src='https://1000logos.net/wp-content/uploads/2017/02/Logo-Instagram.png'
-          alt='logo'
-          />
-         </div>
-      <h1>hello instagram clone</h1>
-      {/*header */}
-       {/* post*/}
-        {/*post */}
-    </div>
+    <div className='App'>
+       <Router>
+        <Routes>
+       <Route exact path="/" element={ <Header />}/>
+       </Routes>
+        
+        
+       </Router>
+       </div>
   );
 }
 
---
+++ src/Components/HeaderComponent/Header.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return(
+        <div className='App_header'>
+        <img className='App_headerImage'
+         src='https://1000logos.net/wp-content/uploads/2017/02/Logo-Instagram.png'
+          alt='logo'
+          />
+         </div>
+    );
+}
+export default Header;
