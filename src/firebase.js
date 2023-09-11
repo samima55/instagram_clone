f76c6bdd652eaa138d13b766904445c7d45b7492
Samima Hassan <samimahassan55@gmail.com>
2023-09-11 20:19:44 +0200

    firebase database connection added

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCPxiTdPhRvalAkLnsmHBL5vYVdmcXIVhU",
+    authDomain: "instagram-clone-a1299.firebaseapp.com",
+    projectId: "instagram-clone-a1299",
+    storageBucket: "instagram-clone-a1299.appspot.com",
+    messagingSenderId: "734195567820",
+    appId: "1:734195567820:web:56c84a50ec334a15a18669",
+    measurementId: "G-8453W9GH8P"
+  };
+
+  export default firebaseConfig;
