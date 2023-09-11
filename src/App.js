8c91f901b3662a0b423962984472970d07777e95
Samima Hassan <samimahassan55@gmail.com>
2023-09-11 19:56:32 +0200

    state is used instead in post

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from './Components/HeaderComponent/Header';
 import Post from './Components/PostComponent/Post';
 function App() {
+       const[posts , setposts] = useState([
+        {username: 'cleverQzi',
+         caption: 'wow didnt know this works' ,
+         imageUrl: 'https://cdn-icons-png.flaticon.com/512/5949/5949033.png'},
+        {
+          username:'LazyGirl' ,
+          caption: 'wanted to sleep more ' ,
+          imageUrl:'https://cdn-icons-png.flaticon.com/512/5949/5949033.png'
+        }
+       ]);
   return (
     <div className='App'>
-      
-      
+  
        <Header/>
-       <Post username='cleverQzi' caption='wow didnt know this works' imageUrl='https://cdn-icons-png.flaticon.com/512/5949/5949033.png'/>
-       <Post/>
-       <Post/>
+      {/*
+        rather than hard coding this , we can use state which is a short term memory store in react
+        and then loop through the array of posts by map
+     <Post username='cleverQzi' caption='wow didnt know this works' imageUrl='https://cdn-icons-png.flaticon.com/512/5949/5949033.png'/>
+       <Post username='LazyGirl' caption='wanted to sleep more ' imageUrl='https://cdn-icons-png.flaticon.com/512/5949/5949033.png'/>
+       <Post username='Smarty' caption='i want to learn new programing lang ' imageUrl='https://cdn-icons-png.flaticon.com/512/5949/5949033.png'/>
       
+       */}
+
+      {
+        posts.map(post=>(
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        ))
+      }
+
        </div>
   );
 }
--- src/Components/PostComponent/Post.js
+++ src/Components/PostComponent/Post.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './Post.css';
 import { Avatar } from '@mui/material';
-function Post(props) {
+function Post({username, caption, imageUrl}) {
   return (
     <div className='post'>
           <div className='post_header'>
           <Avatar
         className='post_avatar'
-        alt={props.username}
+        alt={username}
         src='/static/images/avatar/1.jpg'
         />
-        <h3 className='post_username'>{props.username}</h3>
+        <h3 className='post_username'>{username}</h3>
           </div>
-         <img  className="post_image" src={props.imageUrl} alt='insa post' />
-         <h4 className='post_caption'> <strong>{props.username}</strong> :  {props.caption}</h4>
+         <img  className="post_image" src={imageUrl} alt='insa post' />
+         <h4 className='post_caption'> <strong>{username}</strong> :  {caption}</h4>
     </div>
   )
 }
