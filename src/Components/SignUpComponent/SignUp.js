4e93e950fff4e5e95f1b582c018c5b65662a75d8
Samima Hassan <samimahassan55@gmail.com>
2023-09-16 21:45:26 +0200

    style for signup added

--- src/Components/SignUpComponent/SignUp.js
+++ src/Components/SignUpComponent/SignUp.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import { Button ,Modal, Box } from '@mui/material';
 import { useState } from 'react';
-import './SignUp.css';
-export const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    display:'block',
-    boxShadow: 24,
-    p: 4,
-  };
-
+import "./SignUp.css";
 
   const SignUp = () => {
     const [open, setOpen] =useState(false);
@@ -34,8 +21,8 @@ export const style = {
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
-          <Box sx={style}>
-            <from>
+          <Box className="box">
+            <from className="signUp-form" >
             <img className='App_headerImage'
            src='https://1000logos.net/wp-content/uploads/2017/02/Logo-Instagram.png'
             alt='logo'
